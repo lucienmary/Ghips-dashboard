3b039888a33559bcb9c0933ddf83d5c31994fa65
lucienmary <marylucien1998@gmail.com>
2020-11-25 22:33:38 +0100

    réveil (à fix)

--- public/timer.js
+++ public/timer.js
@@ -1,5 +1,7 @@
-setInterval(timer,1000);
 
+// Horloge.
+// --------
+setInterval(timer,1000);
 
 function addZero(i) {
     if (i < 10) {
@@ -17,3 +19,46 @@ function timer() {
     $('#date').text(date);
     $('#timer strong').text(h + ":" + m + ":" + s);
 }
+
+
+// Set block ALARM (réveil) et fonctionnement.
+// -------------------------------------------
+var alarmActiveList = {};
+var cptBlockClockAlarm = 0;
+var cptBlockClockAlarmActive = 0;
+
+setBlockClockAlarm();
+function setBlockClockAlarm() {
+
+    if (cptBlockClockAlarm == cptBlockClockAlarmActive) {
+        cptBlockClockAlarm++;
+        $('#clock-alarm-list').append(`
+            <li><form class="clock-alarm-block" id="clock-alarm`+cptBlockClockAlarm+`-block">
+            <label for="clock-alarm`+cptBlockClockAlarm+`">Alarme `+cptBlockClockAlarm+`:</label>
+            <input type="time" id="clock-alarm`+cptBlockClockAlarm+`" name="clock-alarm`+cptBlockClockAlarm+`" required>
+            <button type="submit" class="btn btn-secondary clock-alarm-validation">Ok</button>
+            <a class="btn btn-secondary clock-alarm-cancel disabled">Annuler</a>
+            <form></li>
+        `);
+        console.log(cptBlockClockAlarm);
+    }
+
+
+}
+
+$("#clock-alarm"+cptBlockClockAlarm+"-block").submit(function(event){
+    console.log(this.id);
+
+    $('#'+this.id).append('<small>✌️</small>');
+    $('#'+this.id+' .clock-alarm-validation').prop("disabled",true);
+    $('#'+this.id+' .clock-alarm-cancel').removeClass('disabled');
+
+    cptBlockClockAlarmActive++;
+    alarmActiveList['alarm'+cptBlockClockAlarmActive] = $( "input" ).val();
+
+    console.log(alarmActiveList);
+    console.log(event);
+    event.preventDefault();
+
+    setBlockClockAlarm();
+});
